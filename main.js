db554317c54c36f9fd03309655298eec1ffb74db
ernstbeckerneto <ernst.becker.neto@gmail.com>
2023-06-09 21:01:39 -0300

    Made it more accessible for mobile phones

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
 const sim = window.document.getElementById("optionYes")
 const nao = window.document.getElementById("optionNo")
-const text = document.getElementById('text')
 
-const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-if (isMobile) {
-    nao.addEventListener("click", entrar);
-    text.innerHTML = <p>Estou no celular</p>
-  } else {
-    nao.addEventListener("mouseenter", entrar);
-  }
-
+// nao.addEventListener("click", celular);
+// nao.addEventListener("mouseenter", entrar);
 let estado = -1
 
-function entrar(){
+function pc(){
     estado++
     if (estado == 0){
         sim.style.transform="translate(+95px)"
@@ -43,32 +35,53 @@ function entrar(){
     }
 }
 
-function celular(){
+function celular() {
+    // 130 = 60
+    // -325 = -230
     estado++
     if (estado == 0){
-        sim.style.transform="translate(+45px)"
-        nao.style.transform="translate(130px, -150px)"
+        sim.style.transform="translate(+80px)"
+        nao.style.transform="translate(60px, -150px)"
     }
     if (estado == 1){
-        nao.style.transform="translate(130px, +150px)"
+        nao.style.transform="translate(60px, +150px)"
     }
     if (estado == 2){
-        nao.style.transform="translate(-325px, +150px)"
+        nao.style.transform="translate(-230px, +150px)"
     }
     if (estado == 3){
-        nao.style.transform="translate(-325px, -150px)"
+        nao.style.transform="translate(-230px, -150px)"
     }
     if (estado == 4){
-        nao.style.transform="translate(+135px, +150px)"
+        nao.style.transform="translate(+60px, +150px)"
     }
     if (estado == 5){
-        nao.style.transform="translate(+135px, -150px)"
+        nao.style.transform="translate(+60px, -150px)"
     }
     if (estado == 6){
-        nao.style.transform="translate(-325px, +150px)"
+        nao.style.transform="translate(-230px, +150px)"
     }
     if (estado == 7){
-        nao.style.transform="translate(-325px, -150px)"
+        nao.style.transform="translate(-230px, -150px)"
         estado = -1
     }
 }
+
+
+function verificarOrientacao() {
+    const orientation = screen.orientation.type;
+
+    if (orientation.includes("portrait")) {
+      nao.removeEventListener("mouseenter", pc);
+      nao.addEventListener("click", celular);
+    } else {
+      nao.removeEventListener("click", celular);
+      nao.addEventListener("mouseenter", pc);
+    }
+  }
+
+  // Chamar a função inicialmente para configurar a orientação correta
+  verificarOrientacao();
+
+  // Adicionar um ouvinte de evento para detectar alterações na orientação da tela
+  screen.orientation.addEventListener("change", verificarOrientacao);
